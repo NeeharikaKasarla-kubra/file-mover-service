935efe84813c45c6f5f1f5364f4b2a58fcfbdac9
nkasarla <neeharika.kasarla@kubra.com>
2025-04-08 12:02:48 -0700

    PLT1-122_FMS Microfunction PoC

--- src/main/java/com/example/filemover/controller/FileMoverController.java
+++ src/main/java/com/example/filemover/controller/FileMoverController.java
@@ -1,8 +1,6 @@
 package com.example.filemover.controller;
 
 import com.example.filemover.service.FileMoverService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,8 +14,6 @@ public class FileMoverController {
 
     @Autowired
     private FileMoverService fileMoverService;
-    private static final Logger logger = LoggerFactory.getLogger(FileMoverController.class);
-
 
     @PostMapping(path = "/move")
     public ResponseEntity<String> moveFile(@RequestParam String bucket,
